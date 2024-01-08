c852042e4823242e5cf9490aa3eed42fefc4fd95
Sontara444 <sontararajput@gmail.com>
2024-01-08 23:04:22 +0530

    todo-list

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -4,11 +4,18 @@ import './TodoList.css'
 
 const TodoList = () => {
   const [activity, setActivity] = useState('')
+  const [listData, setListData] = useState([])
+
+  function addActivity(){
+        setListData([...listData, activity])
+        console.log(listData)
+  }
   return (
     <>
     <div className='container'>
         <div className='header'>ToDo List</div>
-        <input type="text" placeholder='Add Activity' value={activity}/>
+        <input type="text" placeholder='Add Activity' value={activity} onChange={(e)=>setActivity(e.target.value)}/>
+        <button onClick={(addActivity)}>Add</button>
     </div>
     </>
   )
